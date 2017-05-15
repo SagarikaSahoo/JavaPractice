49a6ecc7a04011fa2482eeb17c4970150fbf1e20
sagarikasahoo <sagarika.sahoo@compassitesinc.com>
2017-05-16 01:46:56 +0530

    End of the constructors

--- src/com/sagarika/tutorial18/constructors/Application.java
+++ src/com/sagarika/tutorial18/constructors/Application.java
@@ -1,9 +1,25 @@
 package com.sagarika.tutorial18.constructors;
 
+class Machine{
+	
+	String name;
+	int code;
+	
+	public Machine(){
+		this.name=name;
+		System.out.println("1st constructor running");
+	}
+	public Machine(int code){
+		this.code = code;
+		System.out.println("2nd constructor running");
+	}
+}
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Machine mn = new Machine();
+		Machine mn1 = new Machine(12);
+		
 
 	}
 
--- src/com/sagarika/tutorial18/constructors/readme.txt
+++ src/com/sagarika/tutorial18/constructors/readme.txt
@@ -1,1 +1,2 @@
 Start time: May 15 time: 1:30 am
+End time: May 15 time 1:44 AM
