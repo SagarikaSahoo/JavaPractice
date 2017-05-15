ec5071a65ee6fbc1c65d4850ac6adecf53a12961
sagarikasahoo <sagarika.sahoo@compassitesinc.com>
2017-05-16 00:49:14 +0530

    End of Tutorial16 method parameter

--- src/com/sagarika/tutorial16/methodparameters/App.java
+++ src/com/sagarika/tutorial16/methodparameters/App.java
@@ -1,5 +1,26 @@
 package com.sagarika.tutorial16.methodparameters;
 
+ class Robot{
+	public void speak(String text){
+		System.out.println(text);
+	}
+	public void height(int distance){
+		System.out.println("The distance " + distance);
+	}
+	public void move(String direction,double dist){
+		System.out.println("the direction is " + direction + " and distance is " + dist);
+	}
+}
 public class App {
+	public static void main(String[] args) {
+		Robot rb = new Robot();
+		rb.speak("Hello Raj");
+		rb.height(6);
+		rb.move("south", 5.2);
+		
+		String greeting = "R u there";
+		rb.speak(greeting);
+		
+	}
 	
 }
--- src/com/sagarika/tutorial16/methodparameters/Readme.txt
+++ src/com/sagarika/tutorial16/methodparameters/Readme.txt
@@ -1,1 +1,2 @@
 Start Time: 15 May 6:55 PM
+End Time: 15 MAy 12:45 AM
