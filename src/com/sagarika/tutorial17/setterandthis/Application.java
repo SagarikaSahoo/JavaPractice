a3d74b8d81c8b2b275aa34be6c17b0dfa80cf112
sagarikasahoo <sagarika.sahoo@compassitesinc.com>
2017-05-16 01:21:43 +0530

    end of setters and this video

--- src/com/sagarika/tutorial17/setterandthis/Application.java
+++ src/com/sagarika/tutorial17/setterandthis/Application.java
@@ -1,10 +1,39 @@
 package com.sagarika.tutorial17.setterandthis;
 
+
+class Frog {
+	private String name;
+	private int age;
+
+	public void setName(String name){
+		this.name=name;
+	}
+	public void setAge(int age){
+		this.age = age;
+				
+	}
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+}
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+      Frog fg = new Frog();
+      //fg.name = "Aj";
+      //fg.age = 8;
+      
+      fg.setName("Simmu");
+      fg.setAge(26);
+     
+      System.out.println(fg.getName());
+      System.out.println(fg.getAge());
 	}
 
 }
---
+++ src/com/sagarika/tutorial17/setterandthis/Readme.txt
@@ -0,0 +1,2 @@
+Start time: May 15 time: 1 am
+End time: MAy 15 time: 1.20 am
