8657b79d7c2c59522847343aa3ba3be179791357
sagarikasahoo <sagarika.sahoo@compassitesinc.com>
2017-05-16 18:13:34 +0530

    start with inheritance video

---
+++ src/com/sagarika/tutorial22/inheritance/Application.java
@@ -0,0 +1,10 @@
+package com.sagarika.tutorial22.inheritance;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/sagarika/tutorial22/inheritance/readme.txt
@@ -0,0 +1,2 @@
+Start time: May 16 time:6:12pm
+End time: May 16 time:
