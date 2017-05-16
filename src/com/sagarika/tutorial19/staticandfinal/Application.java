2053a3bd621fb756b943cec3111cdfd18084e3c2
sagarikasahoo <sagarika.sahoo@compassitesinc.com>
2017-05-16 15:11:06 +0530

    End with static and final

--- src/com/sagarika/tutorial19/staticandfinal/Application.java
+++ src/com/sagarika/tutorial19/staticandfinal/Application.java
@@ -1,9 +1,31 @@
 package com.sagarika.tutorial19.staticandfinal;
-
+class Thing{
+	public final static int luckynumber = 7;
+	public String name;
+	public static String description;
+	
+	public void showName() {
+		System.out.println(description + ":" + name);
+	}
+	public static void showInfo(){
+		System.out.println(description);
+	}
+}
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Thing thing1 = new Thing();
+		Thing thing2 = new Thing();
+		
+		Thing.description = "hey";
+		
+		thing1.name = "hello";
+		thing2.name = "There";
+		
+		thing1.showName();
+		thing2.showName();
+		
+		System.out.println(Thing.luckynumber);
 
 	}
 
---
+++ src/com/sagarika/tutorial19/staticandfinal/readme.txt
@@ -0,0 +1,2 @@
+Start time: May 15 time: 1 pm
+End time: MAy 15 time: 3.09 pm
