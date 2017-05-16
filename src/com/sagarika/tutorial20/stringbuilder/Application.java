41efb7e9b2c53ca801daaf69e2286ad6bd641e11
sagarikasahoo <sagarika.sahoo@compassitesinc.com>
2017-05-16 16:13:54 +0530

    End of the string builder

--- src/com/sagarika/tutorial20/stringbuilder/Application.java
+++ src/com/sagarika/tutorial20/stringbuilder/Application.java
@@ -1,9 +1,34 @@
 package com.sagarika.tutorial20.stringbuilder;
 
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String info = "";
+		info += "My name is saga.";
+		info += " ";
+		info += "I am a coder";
+		
+		System.out.println(info);
+		
+		StringBuilder sb = new StringBuilder("");
+		
+		sb.append("My name is saga");
+		sb.append(" : ");
+		sb.append("I am a looser hehe...");
+		
+		StringBuilder s = new StringBuilder();
+		
+		s.append("My name is khan")
+		.append(" ")
+		.append("i am a runner. ");
+		
+		System.out.println(s.toString());
+		
+		////Formating///////
+		
+		System.out.println("Here is some text. \t that was tab . \n that was new line");
+		System.out.println("More text.");
 
 	}
 
--- src/com/sagarika/tutorial20/stringbuilder/Readme.txt
+++ src/com/sagarika/tutorial20/stringbuilder/Readme.txt
@@ -1,2 +1,2 @@
 Start time: May 16 time: 3:50 pm
-End time: May 16 time: 
+End time: May 16 time: 4:13PM
