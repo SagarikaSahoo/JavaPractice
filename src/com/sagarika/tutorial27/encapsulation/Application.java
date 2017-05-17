0002b376c92f6209d6d4ef839c272f32109e68d9
sagarikasahoo <sagarika.sahoo@compassitesinc.com>
2017-05-17 18:46:03 +0530

    end with encapsulation

---
+++ src/com/sagarika/tutorial27/encapsulation/Application.java
@@ -0,0 +1,33 @@
+package com.sagarika.tutorial27.encapsulation;
+
+class Plant {
+
+	public static final int ID = 7;
+	private String name;
+
+	public String getData() {
+		String data = "some stuff" + calculateGrowthForecast();
+		return data;
+	}
+
+	private int calculateGrowthForecast() {
+		return 9;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+
+public class Application {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/com/sagarika/tutorial27/encapsulation/readme.txt
@@ -0,0 +1,2 @@
+Start time - May 17 time: 6:35PM
+End time - may 17 time 6:42PM
